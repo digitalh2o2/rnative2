dccf1cdfe968b56782c5439092eabc7c56f948b8
digitalh2o <digitalh2o@gmail.com>
2018-05-10 22:37:05 -0500

    Adding inputs for authentication

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
-import { TextInput } from "react-native";
-import { Button, Card, CardSection } from "./common";
+import { Button, Card, CardSection, Input } from "./common";
 
 class LoginForm extends React.Component {
+  state = { email: "" };
+
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput />
+          <Input
+            placeholder="user@gmail.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
         </CardSection>
         <CardSection />
         <CardSection>
